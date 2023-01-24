38d43510e51958ee83ed8eff1a59c72c01e99005
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-24 13:14:22 +0200

    Task 4

--- src/Homework.java
+++ src/Homework.java
@@ -20,6 +20,21 @@ public class Homework {
         task1();
         task2();
         task3();
+        task4();
+
+    }
+
+    private static void task4() {
+
+        System.out.println("Задача 4");
+
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+
+        System.out.println();
 
     }
 
