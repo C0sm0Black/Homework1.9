80b8e7a097dfc84620ddec5c238c8c517cc5791d
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-24 12:51:35 +0200

    Initial commit

---
+++ src/Homework.java
@@ -0,0 +1,23 @@
+public class Homework {
+
+    public static int[] generateRandomArray() {
+
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            arr[i] = random.nextInt(100_000) + 100_000;
+
+        }
+
+        return arr;
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
